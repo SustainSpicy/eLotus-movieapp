5d960aa8b65233902294956be2e69033f9c38e63
sustainspicy <isrealspicy@gmail.com>
2023-10-16 16:56:37 +0100

    final feature update

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -27,16 +27,15 @@ const SliderComponent = ({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 3,
+          slidesToScroll: 3,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 800,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2,
         },
       },
       {
